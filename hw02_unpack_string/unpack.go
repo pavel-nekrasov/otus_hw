ea46663be89f20572f31181f78a3f8b128e6b7c2
Pavel Nekrasov <sileaudivide@gmail.com>
2024-08-14 13:02:06 +0300

    HW2 completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,90 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+type ParseState struct {
+	builder strings.Builder
+	Cur     rune
+	Prev    rune
+	Escaped bool
+}
+
+func Unpack(input string) (string, error) {
+	var state ParseState
+
+	for _, state.Cur = range input {
+		if state.Escaped {
+			if !state.isBackSlash() && !state.isDigit() {
+				return "", ErrInvalidString
+			}
+			state.Escaped = false
+			state.push()
+			continue
+		}
+
+		switch {
+		case state.isBackSlash():
+			state.output()
+			state.Escaped = true
+		case state.isEmpty() && state.isDigit():
+			return "", ErrInvalidString
+		case state.isEmpty():
+			state.push()
+		case state.isDigit():
+			state.outputN()
+		default:
+			state.output()
+			state.push()
+		}
+	}
+
+	state.output()
+
+	return state.string(), nil
+}
+
+func (s *ParseState) isDigit() bool {
+	return s.Cur >= 48 && s.Cur <= 57
+}
+
+func (s *ParseState) isBackSlash() bool {
+	return s.Cur == 92
+}
+
+func (s *ParseState) toDigit() int {
+	return int(s.Cur) - 48
+}
+
+func (s *ParseState) isEmpty() bool {
+	return s.Prev == 0
+}
+
+func (s *ParseState) push() {
+	s.Prev = s.Cur
+}
+
+func (s *ParseState) pop() rune {
+	r := s.Prev
+	s.Prev = 0
+	return r
+}
+
+func (s *ParseState) output() {
+	if !s.isEmpty() {
+		s.builder.WriteString(string(s.pop()))
+	}
+}
+
+func (s *ParseState) outputN() {
+	if !s.isEmpty() {
+		s.builder.WriteString(strings.Repeat(string(s.pop()), s.toDigit()))
+	}
+}
+
+func (s *ParseState) string() string {
+	return s.builder.String()
 }
