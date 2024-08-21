9a1a9fd1e3990f09d4bda6f7ab44e4f4ffed7f25
Pavel Nekrasov <sileaudivide@gmail.com>
2024-08-21 21:47:51 +0300

    HW03 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var reg = regexp.MustCompile(`(\p{L}[\p{L}|-]*)|(-{2,})`)
+
+type item struct {
+	Str   string
+	Count int
+}
+
+type (
+	hashStatitic        map[string]int
+	wordStatistic       []item
+	sortedWordStatistic []item
+)
+
+func Top10(input string) []string {
+	return parse(input).sort().top10()
+}
+
+func parse(input string) wordStatistic {
+	h := make(hashStatitic)
+
+	for _, s := range reg.FindAllString(input, -1) {
+		h[strings.ToLower(s)]++
+	}
+	return h.toWordStatictic()
+}
+
+func (h hashStatitic) toWordStatictic() wordStatistic {
+	s := make(wordStatistic, 0, len(h))
+	for k, v := range h {
+		s = append(s, item{Str: k, Count: v})
+	}
+	return s
+}
+
+func (wordStat wordStatistic) sort() sortedWordStatistic {
+	sort.Slice(wordStat, func(i, j int) bool {
+		return wordStat[i].Count > wordStat[j].Count ||
+			(wordStat[i].Count == wordStat[j].Count && wordStat[i].Str < wordStat[j].Str)
+	})
+
+	return sortedWordStatistic(wordStat)
+}
+
+func (wordStat sortedWordStatistic) top10() []string {
+	results := make([]string, 0, 10)
+
+	for i := 0; i < 10 && i < len(wordStat); i++ {
+		results = append(results, wordStat[i].Str)
+	}
+	return results
 }
