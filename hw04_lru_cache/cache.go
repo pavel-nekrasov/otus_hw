3ce8f47195ec047503bf52bba6e5d24479355b84
Pavel Nekrasov <sileaudivide@gmail.com>
2024-09-04 21:05:02 +0300

    Review fix 3

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -10,20 +10,23 @@ type Cache interface {
 	Clear()
 }
 
+type internalData struct {
+	Key   Key
+	Value interface{}
+}
+
 type lruCache struct {
-	mu          sync.Mutex
-	capacity    int
-	queue       List
-	items       map[Key]*ListItem
-	itemsContra map[*ListItem]Key
+	mu       sync.Mutex
+	capacity int
+	queue    List
+	items    map[Key]*ListItem
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
-		capacity:    capacity,
-		queue:       NewList(),
-		items:       make(map[Key]*ListItem, capacity),
-		itemsContra: make(map[*ListItem]Key, capacity),
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*ListItem, capacity),
 	}
 }
 
@@ -31,13 +34,15 @@ func (c *lruCache) Set(key Key, value interface{}) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
+	data := internalData{Key: key, Value: value}
+
 	if item, ok := c.items[key]; ok {
 		c.queue.MoveToFront(item)
-		item.Value = value
+		item.Value = data
 		return true
 	}
 
-	c.storeItem(key, c.queue.PushFront(value))
+	c.storeItem(key, c.queue.PushFront(data))
 	if c.queue.Len() > c.capacity {
 		c.removeItem(c.queue.Back())
 	}
@@ -51,7 +56,8 @@ func (c *lruCache) Get(key Key) (interface{}, bool) {
 
 	if item, ok := c.items[key]; ok {
 		c.queue.MoveToFront(item)
-		return item.Value, true
+		data, ok := item.Value.(internalData)
+		return data.Value, ok
 	}
 	return nil, false
 }
@@ -62,17 +68,17 @@ func (c *lruCache) Clear() {
 
 	c.queue = NewList()
 	c.items = make(map[Key]*ListItem, c.capacity)
-	c.itemsContra = make(map[*ListItem]Key, c.capacity)
 }
 
 func (c *lruCache) storeItem(key Key, item *ListItem) {
 	c.items[key] = item
-	c.itemsContra[item] = key
 }
 
 func (c *lruCache) removeItem(item *ListItem) {
-	key := c.itemsContra[item]
-	delete(c.items, key)
-	delete(c.itemsContra, item)
-	c.queue.Remove(item)
+	data, ok := item.Value.(internalData)
+
+	if ok {
+		delete(c.items, data.Key)
+		c.queue.Remove(item)
+	}
 }
