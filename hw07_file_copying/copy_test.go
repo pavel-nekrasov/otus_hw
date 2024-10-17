120a5ad92ae069fffd7a5a7662193de87d84f3fa
Pavel Nekrasov <sileaudivide@gmail.com>
2024-10-17 19:59:56 +0300

    review fixes

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -29,7 +29,6 @@ func TestCopyInvalidCases(t *testing.T) {
 func TestCopy(t *testing.T) {
 	tests := []struct {
 		from   string
-		to     string
 		offset int64
 		limit  int64
 
@@ -37,42 +36,36 @@ func TestCopy(t *testing.T) {
 	}{
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  0,
 			limit:   0,
 			example: "./testdata/out_offset0_limit0.txt",
 		},
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  0,
 			limit:   10,
 			example: "./testdata/out_offset0_limit10.txt",
 		},
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  0,
 			limit:   1000,
 			example: "./testdata/out_offset0_limit1000.txt",
 		},
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  0,
 			limit:   10000,
 			example: "./testdata/out_offset0_limit10000.txt",
 		},
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  100,
 			limit:   1000,
 			example: "./testdata/out_offset100_limit1000.txt",
 		},
 		{
 			from:    "./testdata/input.txt",
-			to:      "./out.txt",
 			offset:  6000,
 			limit:   1000,
 			example: "./testdata/out_offset6000_limit1000.txt",
@@ -82,13 +75,17 @@ func TestCopy(t *testing.T) {
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.example, func(t *testing.T) {
-			err := Copy(tc.from, tc.to, tc.offset, tc.limit)
-			defer os.Remove(tc.to)
+			fOut, err := os.CreateTemp("", "out.*.txt")
+			if err != nil {
+				panic(err)
+			}
+			err = Copy(tc.from, fOut.Name(), tc.offset, tc.limit)
+			defer os.Remove(fOut.Name())
 
 			require.NoError(t, err)
 
 			cmp := equalfile.New(nil, equalfile.Options{})
-			result, _ := cmp.CompareFile(tc.to, tc.example)
+			result, _ := cmp.CompareFile(fOut.Name(), tc.example)
 			require.True(t, result)
 		})
 	}
