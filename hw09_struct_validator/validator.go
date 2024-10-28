cac014255f9157d9d76c8660dd7b221417ad364a
Pavel Nekrasov <sileaudivide@gmail.com>
2024-10-28 18:05:37 +0300

    hw09 p1

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,12 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +15,68 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var b strings.Builder
+	for _, e := range v {
+		fmt.Fprintf(&b, "%s: %s", e.Field, e.Err.Error())
+	}
+	return b.String()
+}
+
+type structValidator struct {
+	errors ValidationErrors
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	if v == nil {
+		return nil
+	}
+
+	rValue := reflect.ValueOf(v)
+
+	if rValue.Type().Kind() == reflect.Struct {
+		validator := newValidator()
+		return validator.ValidateStruct(v)
+	}
+
+	return nil
+}
+
+func newValidator() *structValidator {
+	validator := structValidator{
+		errors: make(ValidationErrors, 0),
+	}
+
+	return &validator
+}
+
+func (sv *structValidator) ValidateStruct(value interface{}) error {
+	reflectValue := reflect.ValueOf(value)
+	//reflectType := reflect.TypeOf(value)
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fieldReflectValue := reflectValue.Field(i)
+		//fieldReflectType := reflectType.Field(i)
+
+		if fieldReflectValue.Type().Kind() == reflect.Struct {
+			fieldStructValidator := newValidator()
+			fmt.Println("struct field: ", fieldReflectValue.Elem())
+			fieldErr := fieldStructValidator.ValidateStruct(fieldReflectValue)
+
+			if fieldErr == nil {
+				continue
+			}
+			var validationErrors ValidationErrors
+
+			if errors.As(fieldErr, &validationErrors) {
+				sv.errors = append(sv.errors, validationErrors...)
+			} else {
+				return fieldErr
+			}
+		}
+
+	}
+
+	if len(sv.errors) > 0 {
+		return sv.errors
+	}
 	return nil
 }
