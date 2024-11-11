83d68a9f014bd889260e9449cb7f5814daa72932
Pavel Nekrasov <sileaudivide@gmail.com>
2024-11-11 12:27:37 +0300

    HW 11 completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,84 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+type telnetConfig struct {
+	timeout time.Duration
+	host    string
+	port    int
+}
+
+var config telnetConfig
+
+func init() {
+	flag.DurationVar(&config.timeout, "timeout", 10*time.Second, "timeout for connection")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	readConfig()
+
+	telnetClient := NewTelnetClient(fmt.Sprintf("%v:%v", config.host, config.port), config.timeout, os.Stdin, os.Stdout)
+
+	err := telnetClient.Connect()
+	defer telnetClient.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect: %v\n", err)
+		return
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	done := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case sig := <-sigs:
+				fmt.Printf("Received sig: %v", sig)
+				close(done)
+				return
+			default:
+				err := telnetClient.Send()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Failed to send: %v\n", err)
+					close(done)
+					return
+				}
+				err = telnetClient.Receive()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Failed to receive: %v\n", err)
+					close(done)
+					return
+				}
+			}
+		}
+	}()
+
+	<-done
+}
+
+func readConfig() {
+	var err error
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "Wrong number of arguments: Expected [options] host port\n")
+		os.Exit(1)
+	}
+	config.host = args[0]
+	config.port, err = strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Port must be a number\n")
+		os.Exit(1)
+	}
 }
