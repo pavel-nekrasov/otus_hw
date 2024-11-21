2c0c623e2d96591acf7b58619f20d49aff8c562d
Pavel Nekrasov <sileaudivide@gmail.com>
2024-11-21 11:59:42 +0300

    HW09

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -4,26 +4,57 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
-type ValidationError struct {
-	Field string
-	Err   error
+type (
+	ValidationError struct {
+		Field string
+		Err   error
+	}
+
+	ValidationErrors []ValidationError
+
+	structValidator struct {
+		prefix           string
+		criticalError    error
+		validationErrors ValidationErrors
+	}
+
+/*
+validationRuleInt func(v int, criteria string) error
+
+validationRuleString func(v string, criteria string) error
+*/
+)
+
+var intRules = map[string]func(string, int, string) error{
+	"in":  validateIntInclusion,
+	"max": validateIntMax,
+	"min": validateIntMin,
 }
 
-type ValidationErrors []ValidationError
+var strRules = map[string]func(string, string, string) error{
+	"in":     validateStringInclusion,
+	"len":    validateStringLen,
+	"regexp": validateStringRegex,
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", v.Field, v.Err.Error())
+}
 
 func (v ValidationErrors) Error() string {
 	var b strings.Builder
-	for _, e := range v {
-		fmt.Fprintf(&b, "%s: %s", e.Field, e.Err.Error())
+	for i, e := range v {
+		if i > 0 {
+			b.WriteString("\n")
+		}
+		b.WriteString(e.Error())
 	}
 	return b.String()
-}
-
-type structValidator struct {
-	errors ValidationErrors
 }
 
 func Validate(v interface{}) error {
@@ -31,52 +62,273 @@ func Validate(v interface{}) error {
 		return nil
 	}
 
-	rValue := reflect.ValueOf(v)
+	reflectValue := reflect.ValueOf(v)
 
-	if rValue.Type().Kind() == reflect.Struct {
-		validator := newValidator()
-		return validator.ValidateStruct(v)
+	// разыменовать указатель (если передавалcя указатель)
+	if reflectValue.Type().Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+	}
+
+	if reflectValue.Type().Kind() == reflect.Struct {
+		validator := newStructValidator("")
+		return validator.ValidateStruct(reflectValue.Interface())
 	}
 
 	return nil
 }
 
-func newValidator() *structValidator {
+func newStructValidator(prefix string) *structValidator {
 	validator := structValidator{
-		errors: make(ValidationErrors, 0),
+		prefix:           prefix,
+		validationErrors: make(ValidationErrors, 0),
 	}
 
 	return &validator
 }
 
 func (sv *structValidator) ValidateStruct(value interface{}) error {
-	reflectValue := reflect.ValueOf(value)
-	//reflectType := reflect.TypeOf(value)
-	for i := 0; i < reflectValue.NumField(); i++ {
-		fieldReflectValue := reflectValue.Field(i)
-		//fieldReflectType := reflectType.Field(i)
+	structValue := reflect.ValueOf(value)
+	structType := reflect.TypeOf(value)
 
-		if fieldReflectValue.Type().Kind() == reflect.Struct {
-			fieldStructValidator := newValidator()
-			fmt.Println("struct field: ", fieldReflectValue.Elem())
-			fieldErr := fieldStructValidator.ValidateStruct(fieldReflectValue)
+	for i := 0; i < structType.NumField(); i++ {
+		structField := structType.Field(i)
+		tag := structField.Tag.Get("validate")
 
-			if fieldErr == nil {
-				continue
-			}
-			var validationErrors ValidationErrors
-
-			if errors.As(fieldErr, &validationErrors) {
-				sv.errors = append(sv.errors, validationErrors...)
-			} else {
-				return fieldErr
-			}
+		if tag == "" {
+			continue
 		}
 
+		switch structField.Type.Kind() {
+		case reflect.Int, reflect.String:
+			sv.handleScalar(structField, tag, structValue.Field(i))
+		case reflect.Slice, reflect.Array:
+			sv.handleVector(structField, tag, structValue.Field(i))
+		case reflect.Struct:
+			sv.handleStruct(structField, tag, structValue.Field(i))
+		case reflect.Ptr:
+			sv.handleStructPtr(structField, tag, structValue.Field(i))
+		default:
+		}
+		if sv.criticalError != nil {
+			return sv.criticalError
+		}
 	}
 
-	if len(sv.errors) > 0 {
-		return sv.errors
+	if len(sv.validationErrors) > 0 {
+		return sv.validationErrors
 	}
 	return nil
+}
+
+func (sv *structValidator) fullFieldName(fieldName string) string {
+	if sv.prefix == "" {
+		return fieldName
+	}
+	return fmt.Sprintf("%v.%v", sv.prefix, fieldName)
+}
+
+func (sv *structValidator) handleScalar(field reflect.StructField, tag string, value reflect.Value) {
+	fullFieldName := sv.fullFieldName(field.Name)
+
+	switch field.Type.Kind() {
+	case reflect.Int:
+		sv.filterErr(validateTags(fullFieldName, tag, int(value.Int()), intRules))
+	case reflect.String:
+		sv.filterErr(validateTags(fullFieldName, tag, value.String(), strRules))
+	}
+}
+
+func (sv *structValidator) handleVector(field reflect.StructField, tag string, value reflect.Value) {
+	fullFieldName := sv.fullFieldName(field.Name)
+
+	switch field.Type.Elem().Kind() {
+	case reflect.Int:
+		for _, v := range value.Interface().([]int) {
+			sv.filterErr(validateTags(fullFieldName, tag, v, intRules))
+			if sv.criticalError != nil {
+				return
+			}
+		}
+	case reflect.String:
+		for _, v := range value.Interface().([]string) {
+			sv.filterErr(validateTags(fullFieldName, tag, v, strRules))
+			if sv.criticalError != nil {
+				return
+			}
+		}
+	}
+}
+
+func (sv *structValidator) handleStruct(field reflect.StructField, tag string, value reflect.Value) {
+	if tag != "nested" {
+		return
+	}
+	fullFieldName := sv.fullFieldName(field.Name)
+	innerValidator := newStructValidator(fullFieldName)
+	sv.filterErr(innerValidator.ValidateStruct(value.Interface()))
+}
+
+func (sv *structValidator) handleStructPtr(field reflect.StructField, tag string, value reflect.Value) {
+	if tag != "nested" {
+		return
+	}
+	if field.Type.Elem().Kind() == reflect.Struct {
+		fullFieldName := sv.fullFieldName(field.Name)
+		innerValidator := newStructValidator(fullFieldName)
+		sv.filterErr(innerValidator.ValidateStruct(value.Elem().Interface()))
+	}
+}
+
+func (sv *structValidator) filterErr(err error) {
+	if err == nil {
+		return
+	}
+
+	var validationErrors ValidationErrors
+	if errors.As(err, &validationErrors) {
+		sv.validationErrors = append(sv.validationErrors, validationErrors...)
+		return
+	}
+	sv.criticalError = err
+}
+
+func validateTags[T comparable](field string, tag string, value T, validators map[string]func(string, T, string) error) error {
+	validationErrors := make(ValidationErrors, 0)
+	rules := strings.Split(tag, "|")
+	for _, rule := range rules {
+		err := validateTag(field, rule, value, validators)
+		if err == nil {
+			continue
+		}
+		var validationErr ValidationError
+		if errors.As(err, &validationErr) {
+			validationErrors = append(validationErrors, validationErr)
+			continue
+		}
+		return err
+	}
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
+	return nil
+}
+
+func validateTag[T comparable](field string, rule string, value T, validators map[string]func(string, T, string) error) error {
+	ruleParts := strings.Split(rule, ":")
+	if len(ruleParts) != 2 {
+		return fmt.Errorf("%v: wrong notation: bad format %v", field, rule)
+	}
+	validator, ok := validators[ruleParts[0]]
+	if !ok {
+		return fmt.Errorf("%v: wrong notation: unknown rule \"%v\"", field, ruleParts[0])
+	}
+	return validator(field, value, ruleParts[1])
+}
+
+func validateStringLen(field string, value string, criteria string) error {
+	intLen, err := strconv.Atoi(criteria)
+	if err != nil {
+		return fmt.Errorf("%v: wrong notation: len \"%v\" is not an integer", field, criteria)
+	}
+
+	if len(value) != intLen {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong len %v, expected %v", len(value), intLen),
+		}
+	}
+
+	return nil
+}
+
+func validateStringInclusion(field string, value string, criteria string) error {
+	expectedValues := strings.Split(criteria, ",")
+
+	if !sliceContains(expectedValues, value) {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong value \"%v\", should be one of %v", value, expectedValues),
+		}
+	}
+
+	return nil
+}
+
+func validateStringRegex(field string, value string, criteria string) error {
+	r, err := regexp.Compile(criteria)
+	if err != nil {
+		return fmt.Errorf("%v: wrong notation: regex \"%v\" is incorrect", field, criteria)
+	}
+
+	if !r.Match([]byte(value)) {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong value \"%v\"", value),
+		}
+	}
+
+	return nil
+}
+
+func validateIntInclusion(field string, value int, criteria string) error {
+	expectedValuesS := strings.Split(criteria, ",")
+
+	expectedValues := make([]int, 0)
+	for _, s := range expectedValuesS {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return fmt.Errorf("%v: wrong notation: \"%v\" is not an integer", field, s)
+		}
+		expectedValues = append(expectedValues, i)
+	}
+
+	if !sliceContains(expectedValues, value) {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong value %v, should be one of %v", value, expectedValues),
+		}
+	}
+
+	return nil
+}
+
+func validateIntMax(field string, value int, criteria string) error {
+	maxCriteria, err := strconv.Atoi(criteria)
+	if err != nil {
+		return fmt.Errorf("%v: wrong notation: max \"%v\" is not an integer", field, criteria)
+	}
+
+	if value > maxCriteria {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong value %v, should less or equal to %v", value, criteria),
+		}
+	}
+
+	return nil
+}
+
+func validateIntMin(field string, value int, criteria string) error {
+	minCriteria, err := strconv.Atoi(criteria)
+	if err != nil {
+		return fmt.Errorf("%v: wrong notation: min \"%v\" is not an integer", field, criteria)
+	}
+
+	if value < minCriteria {
+		return ValidationError{
+			Field: field,
+			Err:   fmt.Errorf("wrong value %v, should greater or equal to %v", value, criteria),
+		}
+	}
+
+	return nil
+}
+
+func sliceContains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -34,6 +36,41 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	InnerStruct struct {
+		InnerVal1 string `validate:"len:7"`
+	}
+
+	OuterStruct1 struct {
+		OuterVal1 string `validate:"len:5"`
+		Inner     InnerStruct
+		OuterVal2 string `validate:"len:5"`
+	}
+
+	OuterStruct2 struct {
+		OuterVal1 string      `validate:"len:5"`
+		Inner     InnerStruct `validate:"nested"`
+		OuterVal2 string      `validate:"len:5"`
+	}
+
+	OuterStruct3 struct {
+		OuterVal1 string       `validate:"len:5"`
+		Inner     *InnerStruct `validate:"nested"`
+		OuterVal2 string       `validate:"len:5"`
+	}
+
+	BadStruct1 struct {
+		Va1 string `validate:"len:xxx"`
+	}
+	BadStruct2 struct {
+		Va1 int `validate:"min:10|max:xxx"`
+	}
+	BadStruct3 struct {
+		Va1 int `validate:"len:11"`
+	}
+	BadStruct4 struct {
+		Va1 int `validate:"in:100,200,xxx"`
+	}
 )
 
 func TestValidate(t *testing.T) {
@@ -42,10 +79,41 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in:          User{ID: "12345", Name: "somename", Age: 15, Email: "sssddd", Role: "stuff"},
+			expectedErr: fmt.Errorf("ID: wrong len 5, expected 36\nAge: wrong value 15, should greater or equal to 18\nEmail: wrong value \"sssddd\""),
 		},
-		// ...
-		// Place your code here.
+		{
+			in:          User{ID: "12345", Name: "somename", Age: 125, Email: "user@example.com", Role: "admin"},
+			expectedErr: fmt.Errorf("ID: wrong len 5, expected 36\nAge: wrong value 125, should less or equal to 50"),
+		},
+		{
+			in:          User{ID: "12345", Name: "somename", Age: 45, Email: "user@example.com", Role: "unknown"},
+			expectedErr: fmt.Errorf("ID: wrong len 5, expected 36\nRole: wrong value \"unknown\", should be one of [admin stuff]"),
+		},
+		{
+			in:          User{ID: "012345678901234567890123456789012345", Name: "somename", Age: 45, Email: "user@example.com", Role: "stuff"},
+			expectedErr: nil,
+		},
+		{
+			in:          App{Version: "12345"},
+			expectedErr: nil,
+		},
+		{
+			in:          App{Version: "1234"},
+			expectedErr: fmt.Errorf("Version: wrong len 4, expected 5"),
+		},
+		{
+			in:          Token{Header: []byte{1, 2, 3}},
+			expectedErr: nil,
+		},
+		{
+			in:          Response{Code: 500},
+			expectedErr: nil,
+		},
+		{
+			in:          Response{Code: 100, Body: "somevalue"},
+			expectedErr: fmt.Errorf("Code: wrong value 100, should be one of [200 404 500]"),
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +121,95 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.EqualError(t, err, tt.expectedErr.Error())
+			}
+		})
+	}
+}
+
+func TestNestedWithPointersValidate(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in:          OuterStruct1{OuterVal1: "xxx", OuterVal2: "yyyyyyy", Inner: InnerStruct{InnerVal1: "zzzz"}},
+			expectedErr: fmt.Errorf("OuterVal1: wrong len 3, expected 5\nOuterVal2: wrong len 7, expected 5"),
+		},
+		{
+			in:          &OuterStruct1{OuterVal1: "xxx", OuterVal2: "yyyyyyy", Inner: InnerStruct{InnerVal1: "zzzz"}},
+			expectedErr: fmt.Errorf("OuterVal1: wrong len 3, expected 5\nOuterVal2: wrong len 7, expected 5"),
+		},
+		{
+			in:          OuterStruct2{OuterVal1: "xxx", OuterVal2: "yyyyyyy", Inner: InnerStruct{InnerVal1: "zzzz"}},
+			expectedErr: fmt.Errorf("OuterVal1: wrong len 3, expected 5\nInner.InnerVal1: wrong len 4, expected 7\nOuterVal2: wrong len 7, expected 5"),
+		},
+		{
+			in:          OuterStruct3{OuterVal1: "xxx", OuterVal2: "yyyyyyy", Inner: &InnerStruct{InnerVal1: "zzzz"}},
+			expectedErr: fmt.Errorf("OuterVal1: wrong len 3, expected 5\nInner.InnerVal1: wrong len 4, expected 7\nOuterVal2: wrong len 7, expected 5"),
+		},
+		{
+			in:          OuterStruct3{OuterVal1: "xxxxx", OuterVal2: "yyyyy", Inner: &InnerStruct{InnerVal1: "zzzzzzz"}},
+			expectedErr: nil,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.EqualError(t, err, tt.expectedErr.Error())
+			}
+		})
+	}
+}
+
+func TestBadNotationSyntax(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in:          BadStruct1{Va1: "xxx"},
+			expectedErr: fmt.Errorf("Va1: wrong notation: len \"xxx\" is not an integer"),
+		},
+		{
+			in:          BadStruct2{Va1: 15},
+			expectedErr: fmt.Errorf("Va1: wrong notation: max \"xxx\" is not an integer"),
+		},
+		{
+			in:          BadStruct3{Va1: 15},
+			expectedErr: fmt.Errorf("Va1: wrong notation: unknown rule \"len\""),
+		},
+		{
+			in:          BadStruct4{Va1: 100},
+			expectedErr: fmt.Errorf("Va1: wrong notation: \"xxx\" is not an integer"),
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.EqualError(t, err, tt.expectedErr.Error())
+			}
 		})
 	}
 }
