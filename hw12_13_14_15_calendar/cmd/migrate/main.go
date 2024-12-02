b20e363952f842ba2e366a3ec2132a0ee10816c1
Pavel Nekrasov <sileaudivide@gmail.com>
2024-12-02 18:51:27 +0300

    HW13 migration to separate executable

--- hw12_13_14_15_calendar/cmd/migrate/main.go
+++ hw12_13_14_15_calendar/cmd/migrate/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/pavel-nekrasov/otus_hw/hw12_13_14_15_calendar/internal/app"
 	"github.com/pavel-nekrasov/otus_hw/hw12_13_14_15_calendar/internal/config"
 	"github.com/pavel-nekrasov/otus_hw/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/pavel-nekrasov/otus_hw/hw12_13_14_15_calendar/internal/server/http"
 )
 
 var configFile string
@@ -44,25 +42,11 @@ func main() {
 	}
 	defer storage.Close(ctx)
 
-	calendar := app.New(logg, storage)
-	server := internalhttp.NewServer(config.HTTP.Host, config.HTTP.Port, logg, calendar)
-
-	go func() {
-		<-ctx.Done()
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		defer cancel()
-
-		if err := server.Stop(ctx); err != nil {
-			logg.Error("failed to stop http server: " + err.Error())
-		}
-	}()
-
-	logg.Info("calendar is running...")
-
-	if err := server.Start(ctx); err != nil {
-		logg.Error("failed to start http server: " + err.Error())
-		cancel()
+	logg.Info("Appying migrations...")
+	err = storage.Migrate(ctx, "migrations")
+	if err != nil {
+		logg.Error(fmt.Sprintf("Failed to apply migrations: %v", err))
 		os.Exit(1)
 	}
+	logg.Info("Done")
 }
